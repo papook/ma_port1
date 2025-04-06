7430159deecc505ca8500612ae29e0bb1dba1b9a
papook <50879652+papook@users.noreply.github.com>
2025-04-06 12:50:50 +0200

    added welcome button

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,37 @@ class MainPage extends StatelessWidget {
           leading: Icon(Icons.home),
           backgroundColor: Color.fromARGB(255, 23, 60, 105),
         ),
+        body: Container(
+          color: Colors.white,
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Center(
+                child: Container(
+                  height: 100,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 106, 0),
+                    border: Border.all(color: Colors.black, width: 5),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Welcome",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontFamily: "Segoe UI",
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
