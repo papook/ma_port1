ada2c1b638e0582b12d6ac341fd338700aef1c10
papook <50879652+papook@users.noreply.github.com>
2025-04-06 12:28:59 +0200

    implemented the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MainPage());
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "First Portfolio Exam",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          leading: Icon(Icons.home),
+          backgroundColor: Color.fromARGB(255, 23, 60, 105),
+        ),
+      ),
+    );
+  }
+}
